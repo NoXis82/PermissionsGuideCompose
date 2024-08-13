86a30641b508d6ce4d6bcbd93162304528ac3983
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-13 23:19:24 +0300

    init commit

--- app/src/main/java/com/noxis/permissionsguidecompose/MainActivity.kt
+++ app/src/main/java/com/noxis/permissionsguidecompose/MainActivity.kt
@@ -1,47 +1,110 @@
 package com.noxis.permissionsguidecompose
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.noxis.permissionsguidecompose.ui.theme.PermissionsGuideComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val permissionsToRequest = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CALL_PHONE
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PermissionsGuideComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val viewModel = viewModel<MyViewModel>()
+                val dialogQueue = viewModel.visiblePermissionDialogQueue
+                val cameraPermissionResultLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { isGranted ->
+                        viewModel.onPermissionResult(
+                            permission = Manifest.permission.CAMERA,
+                            isGranted = isGranted
+                        )
+                    }
+                )
+
+                val multiplePermissionResultLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestMultiplePermissions(),
+                    onResult = { perms ->
+                        permissionsToRequest.forEach { permission ->
+                            viewModel.onPermissionResult(
+                                permission = permission,
+                                isGranted = perms[permission] == true
+                            )
+                        }
+                    }
+                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        cameraPermissionResultLauncher.launch(
+                            Manifest.permission.CAMERA
+                        )
+                    }) {
+                        Text(text = "Request one permission")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        multiplePermissionResultLauncher.launch(permissionsToRequest)
+                    }) {
+                        Text(text = "Request multiple permission")
+                    }
+                }
+
+                dialogQueue.reversed().forEach { permission ->
+                    PermissionDialog(
+                        permissionTextProvider = when (permission) {
+                            Manifest.permission.CAMERA -> CameraPermissionTextProvider()
+                            Manifest.permission.CALL_PHONE -> PhoneCallPermissionTextProvider()
+                            Manifest.permission.RECORD_AUDIO -> RecordAudioPermissionTextProvider()
+                            else -> return@forEach
+                        },
+                        isPermanentlyDeclined = !shouldShowRequestPermissionRationale(permission),
+                        onDismiss = viewModel::dismissDialog,
+                        onOkClick = {
+                            viewModel.dismissDialog()
+                            multiplePermissionResultLauncher.launch(arrayOf(permission))
+                        },
+                        onGoToAppSettingsClick = ::openAppSettings
                     )
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PermissionsGuideComposeTheme {
-        Greeting("Android")
-    }
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
 }
